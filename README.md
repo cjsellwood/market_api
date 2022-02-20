ced77c13bce2ad5cca46512d350847e9d1cb6411
cjsellwood <c.sellwood22@gmail.com>
2022-02-21 10:16:48 +1100

    Add method to create sql database

---
+++ README.md
@@ -0,0 +1,21 @@
+# Market Api
+
+Backend for market
+
+### npm start
+Run development server
+
+### npm test
+Run tests locally
+
+### npm run pretest
+Create database for use in tests
+
+### npm run test:actions
+Runs tests once for github actions
+
+### npm run test:coverage
+Runs test locally and show coverage
+
+### npm run seed
+Seed sql database
---
+++ db/createDb.ts
@@ -0,0 +1,30 @@
+const pgtools = require("pgtools");
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  user: "postgres",
+  password: "password",
+  port: 5432,
+  host: "localhost",
+};
+
+pgtools.createdb(config, "market_api", (err: Error, res: unknown) => {
+  if (err) {
+    // If already exists ignore and continue
+    if (err.name !== "duplicate_database") {
+      process.exit(-1);
+    }
+  }
+
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  const sqlConnection = async () => {
+    await pool.end();
+  };
+
+  sqlConnection();
+});
