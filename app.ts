acdf73c76d8a6bb1a8da04c85500d8aa4fcb84d7
cjsellwood <c.sellwood22@gmail.com>
2022-02-21 13:53:06 +1100

    Setup error handling and async error catcher

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
+import StatusError from "./utils/StatusError";
 
 const app = express();
 
@@ -11,5 +12,13 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use(express.json());
 app.use(morgan("dev", { skip: () => process.env.NODE_ENV === "test" }));
+
+// Error handling
+app.use(
+  (error: StatusError, req: Request, res: Response, next: NextFunction) => {
+    console.log("ERROR: ", error.status, " ", error.message);
+    res.status(error.status || 500).send(error.message);
+  }
+);
 
 export default app;
